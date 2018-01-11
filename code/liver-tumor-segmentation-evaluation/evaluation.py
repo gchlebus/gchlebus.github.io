20a1f50812fff80fb2be1e1478c876ba1f69067e
gchlebus <gchlebus@gmail.com>
2018-01-11 11:18:05 +0100

    Continue development of the evaluate method.

--- code/liver-tumor-segmentation-evaluation/evaluation.py
+++ code/liver-tumor-segmentation-evaluation/evaluation.py
@@ -4,25 +4,33 @@ __author__ = 'gchlebus'
 import numpy as np
 
 def evaluate(refmask, testmask, th=0.5):
-  if not np.any(testmask):
-    return 0, 0
-  if not np.any(refmask) and np.any(testmask):
-    return 0, len(unique(testmask))
-
   tp = 0
-  fp = 0
-  for idx in unique(testmask):
-    current_test_mask = np.zeros_like(testmask)
-    current_test_mask[testmask == idx] = 1
+  correspondences = []
+  if not np.any(testmask) or (not np.any(refmask) and np.any(testmask)):
+    return tp, len(unique(testmask)), correspondences
     
+  for idx in unique(testmask):
+    current_test_mask = np.where(testmask == idx, 1, 0)
+    #print('current_test_mask', current_test_mask)
+    if not np.any(current_test_mask):
+      continue
     current_ref_mask = np.zeros_like(refmask)
     for i in unique(current_test_mask * refmask):
       current_ref_mask[refmask == i] = 1
+    
+    for i in unique(current_ref_mask * testmask):
+      current_test_mask[testmask == i] = 1
+    
+
+    #print('jaccard current_test_mask', current_test_mask)
+    #print('jaccard current_ref_mask', current_ref_mask)
     if jaccard(current_ref_mask, current_test_mask) > th:
       tp += len(unique(current_test_mask * refmask))
-    else:
-      fp += 1
-  return tp, fp
+      correspondences.append([unique(refmask * current_ref_mask).tolist(), unique(testmask * current_test_mask).tolist()])
+      testmask[current_test_mask==1] = 0
+      #print('testmask', testmask)
+    
+  return tp, len(unique(testmask)), correspondences
 
 def unique(array):
   ret = np.unique(array)
@@ -33,6 +41,7 @@ def jaccard(refmask, testmask):
   assert not np.any(testmask > 1)
   intersection = np.sum(refmask * testmask)
   union = np.sum(refmask + testmask) - intersection
+  #print('jaccard = ', intersection / union)
   return intersection / union
 
 if __name__ == '__main__':
--- code/liver-tumor-segmentation-evaluation/test_evaluation.py
+++ code/liver-tumor-segmentation-evaluation/test_evaluation.py
@@ -5,17 +5,18 @@ from evaluation import evaluate
 import numpy as np
 
 def test_zero_input():
-  tp, fp = evaluate(np.zeros(shape=(1,1)), np.zeros(shape=(1,1)))
+  tp, fp, correspondences = evaluate(np.zeros(shape=(1,1)), np.zeros(shape=(1,1)))
   assert tp == 0
   assert fp == 0
 
+
 def test_empty_refmask():
-  tp, fp = evaluate(np.zeros(shape=(1,1)), np.ones(shape=(1,1)))
+  tp, fp, correspondences = evaluate(np.zeros(shape=(1,1)), np.ones(shape=(1,1)))
   assert tp == 0
   assert fp == 1
 
 def test_empty_testmask():
-  tp, fp = evaluate(np.ones(shape=(1,1)), np.zeros(shape=(1,1)))
+  tp, fp, correspondences = evaluate(np.ones(shape=(1,1)), np.zeros(shape=(1,1)))
   assert tp == 0
   assert fp == 0
 
@@ -29,7 +30,7 @@ def test_one_ref_one_test_1():
   testmask = np.zeros(shape=(2,2))
   refmask[0,0] = 1
   testmask[0,0] = 1
-  tp, fp = evaluate(refmask, testmask)
+  tp, fp, correspondences = evaluate(refmask, testmask)
   assert tp == 1
   assert fp == 0
 
@@ -43,7 +44,7 @@ def test_one_ref_one_test_2():
   testmask = np.zeros(shape=(2,2))
   refmask[0,0] = 1
   testmask[1,0] = 1
-  tp, fp = evaluate(refmask, testmask)
+  tp, fp, correspondences = evaluate(refmask, testmask)
   assert tp == 0
   assert fp == 1
 
@@ -57,7 +58,7 @@ def test_one_ref_one_test_1():
   refmask = np.zeros(shape=(2,2))
   testmask = np.ones(shape=(2,2))
   refmask[0,0] = 1
-  tp, fp = evaluate(refmask, testmask)
+  tp, fp, correspondences = evaluate(refmask, testmask)
   assert tp == 0
   assert fp == 1
 
@@ -74,7 +75,7 @@ def test_one_ref_two_test():
   refmask[0,0] = 1 
   testmask[0,0] = 1
   testmask[2,2] = 2
-  tp, fp = evaluate(refmask, testmask)
+  tp, fp, correspondences = evaluate(refmask, testmask)
   assert tp == 1
   assert fp == 1
 
@@ -92,7 +93,7 @@ def test_two_ref_two_test_1():
   refmask[2,2] = 2
   testmask[0,0] = 1
   testmask[2,2] = 2
-  tp, fp = evaluate(refmask, testmask)
+  tp, fp, correspondences = evaluate(refmask, testmask)
   assert tp == 2
   assert fp == 0
 
@@ -113,6 +114,92 @@ def test_two_ref_one_test_2():
   testmask[2,0] = 0
   testmask[1,1] = 0
   testmask[2,1] = 0
-  tp, fp =  evaluate(refmask, testmask)
+  tp, fp, correspondences =  evaluate(refmask, testmask)
   assert tp == 2
   assert fp == 0
+
+def test_one_ref_two_test_2():
+  '''
+  ref     test
+  1 1 1   1 1 1
+  1 1 1   0 0 0
+  1 1 1   2 2 2
+  '''
+  refmask = np.ones(shape=(3,3))
+  testmask = np.array([
+    [1, 1, 1],
+    [0, 0, 0],
+    [2, 2, 2]
+  ])
+  tp, fp, correspondences =  evaluate(refmask, testmask)
+  assert tp == 1
+  assert fp == 0
+  assert correspondences == [[[1],[1,2]]]
+
+def test_two_ref_two_test():
+  '''
+  ref     test
+  1 1 1   1 1 1
+  1 1 1   0 0 0
+  1 1 1   2 2 2
+  0 0 0   2 2 2
+  2 2 2   2 2 2
+  '''
+  refmask = np.array([
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+    [2, 2, 2]
+  ])
+  testmask = np.array([
+    [1, 1, 1],
+    [0, 0, 0],
+    [2, 2, 2],
+    [2, 2, 2],
+    [2, 2, 2],
+  ])
+  tp, fp, correspondences =  evaluate(refmask, testmask)
+  assert tp == 2
+  assert fp == 0
+  assert correspondences == [[[1,2],[1,2]]]
+
+def test_three_ref_three_test():
+  '''
+  ref     test
+  1 1 1   1 1 1
+  1 1 1   0 0 0
+  1 1 1   2 2 2
+  0 0 0   2 2 2
+  2 2 2   2 2 2
+  2 2 2   2 2 2 
+  0 0 0   0 0 0
+  0 0 0   3 3 3
+  3 3 3   0 0 0
+  '''
+  refmask = np.array([
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+    [2, 2, 2],
+    [2, 2, 2],
+    [0, 0, 0],
+    [0, 0, 0],
+    [3, 3, 3],
+  ])
+  testmask = np.array([
+    [1, 1, 1],
+    [0, 0, 0],
+    [2, 2, 2],
+    [2, 2, 2],
+    [2, 2, 2],
+    [2, 2, 2],
+    [0, 0, 0],
+    [3, 3, 3],
+    [0, 0, 0]
+  ])
+  tp, fp, correspondences =  evaluate(refmask, testmask)
+  assert tp == 2
+  assert fp == 1
+  assert correspondences == [[[1,2],[1,2]]]
