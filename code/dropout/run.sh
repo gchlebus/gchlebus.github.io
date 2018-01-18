545001e385191cb65302e413a3587ec8e9990bcc
gchlebus <gchlebus@gmail.com>
2018-01-18 12:21:28 +0100

    Add run script.

---
+++ code/dropout/run.sh
@@ -0,0 +1,9 @@
+#! /bin/zsh
+
+python dropout_network.py
+
+python dropout_network.py --dropout
+
+python dropout_network.py --dropout --max_norm 10
+
+python dropout_network.py --dropout --max_norm 50
