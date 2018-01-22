3332b91da0d432730cb5b66515f5f156f4afbc1e
gchlebus <gchlebus@gmail.com>
2018-01-22 21:54:23 +0100

    Add function to create a session w/o optimizations. Pass gate_gradients=True to gradients functions.

--- code/profiling-tf-models/profiler.py
+++ code/profiling-tf-models/profiler.py
@@ -3,6 +3,7 @@ __author__ = 'gchlebus'
 
 import tensorflow as tf
 from tensorflow.python.client import timeline
+from tensorflow.core.protobuf import rewriter_config_pb2
 import numpy as np
 from u_net import UNet, GradientType
 import mem_util
@@ -13,48 +14,61 @@ class OutputType():
   STDOUT = 2
   NONE = 3
 
-def profile(img_size=128, batch_size=1, filters=16, n_conv=2, dropout=0.5, batch_norm=False,
-  output_type=OutputType.NONE, gradient_type=GradientType.PLAIN_ADAM):
+def get_session(disable_optimizer):
+  print('DISABLE OPTIMIZER:', str(disable_optimizer).upper())
+  if disable_optimizer:
+    optimizer_options = tf.OptimizerOptions(opt_level=tf.OptimizerOptions.L0)
+    config = tf.ConfigProto(operation_timeout_in_ms=150000, graph_options=tf.GraphOptions(optimizer_options=optimizer_options))
+    config.graph_options.rewrite_options.constant_folding = rewriter_config_pb2.RewriterConfig.OFF
+    config.graph_options.place_pruned_graph = True
+    return tf.Session(config=config)
+  else:
+    return tf.Session()
+
+
+def profile(img_size=128, batch_size=1, filters=64, n_conv=5, dropout=0.5, batch_norm=False,
+  output_type=OutputType.NONE, gradient_type=GradientType.PLAIN_ADAM, disable_optimizer=False):
   tf.reset_default_graph()
   unet = UNet(filters, n_conv, dropout, batch_norm, gradient_type=gradient_type)
-  with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    input_batch = np.random.rand(batch_size, img_size, img_size, 1)
-    output_batch = np.random.rand(batch_size, img_size, img_size, 2)
+  sess = get_session(disable_optimizer)
+  
+  sess.run(tf.global_variables_initializer())
+  input_batch = np.random.rand(batch_size, img_size, img_size, 1)
+  output_batch = np.random.rand(batch_size, img_size, img_size, 2)
 
-    run_metadata = tf.RunMetadata()
-    options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-    unet.train(sess, input_batch, output_batch, options=options, run_metadata=run_metadata)
+  run_metadata = tf.RunMetadata()
+  options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+  unet.train(sess, input_batch, output_batch, options=options, run_metadata=run_metadata)
 
-    builder = (tf.profiler.ProfileOptionBuilder()
-      .with_max_depth(6)
-      .select(['micros', 'bytes', 'params', 'float_ops'])
-      .order_by('peak_bytes'))
-    if output_type == OutputType.FILE:
-      builder = builder.with_file_output('profile_output')
-    elif output_type == OutputType.TIMELINE:
-      builder = builder.with_timeline_output('timeline_output')
-    elif output_type == OutputType.STDOUT:
-      builder = builder.with_stdout_output()
-    else:
-      builder = builder.with_empty_output()
-    options = builder.build()
-    result = tf.profiler.profile(tf.get_default_graph(), run_meta=run_metadata, cmd="scope",
-                        options=options)
-    print(mem_util.peak_memory(run_metadata))
-    #print(run_metadata)
-    #tl = timeline.Timeline(run_metadata.step_stats)
-    #print(tl.generate_chrome_trace_format(show_memory=True))
-    #trace_file = tf.gfile.Open(name='timeline_out', mode='w')
-    #trace_file.write(tl.generate_chrome_trace_format(show_memory=True))
+  builder = (tf.profiler.ProfileOptionBuilder()
+    .with_max_depth(6)
+    .select(['micros', 'bytes', 'params', 'float_ops'])
+    .order_by('peak_bytes'))
+  if output_type == OutputType.FILE:
+    builder = builder.with_file_output('profile_output')
+  elif output_type == OutputType.TIMELINE:
+    builder = builder.with_timeline_output('timeline_output')
+  elif output_type == OutputType.STDOUT:
+    builder = builder.with_stdout_output()
+  else:
+    builder = builder.with_empty_output()
+  options = builder.build()
+  result = tf.profiler.profile(tf.get_default_graph(), run_meta=run_metadata, cmd="scope",
+                      options=options)
+  print(mem_util.peak_memory(run_metadata))
+  #print(run_metadata)
+  #tl = timeline.Timeline(run_metadata.step_stats)
+  #print(tl.generate_chrome_trace_format(show_memory=True))
+  #trace_file = tf.gfile.Open(name='timeline_out', mode='w')
+  #trace_file.write(tl.generate_chrome_trace_format(show_memory=True))
 
-    ret = dict()
-    #print(result)
-    ret['total'] = get_profiling_results(result)
-    for name in 'train model'.split():
-      child = [c for c in result.children if c.name == name][0]
-      ret[name] = get_profiling_results(child)
-    return ret
+  ret = dict()
+  #print(result)
+  ret['total'] = get_profiling_results(result)
+  for name in 'train model'.split():
+    child = [c for c in result.children if c.name == name][0]
+    ret[name] = get_profiling_results(child)
+  return ret
 
 def get_profiling_results(node):
   ret = dict()
--- code/profiling-tf-models/u_net.py
+++ code/profiling-tf-models/u_net.py
@@ -5,11 +5,20 @@ import tensorflow as tf
 from memory_saving_gradients import gradients_memory, gradients_speed, gradients_collection
 
 class GradientType():
-  PLAIN_ADAM = 0
-  TF_GRADIENTS = 1
-  GRADIENTS_SPEED = 2
-  GRADIENTS_MEMORY = 3
-  GRADIENTS_COLLECTION = 4
+  PLAIN_ADAM = 'PLAIN_ADAM'
+  TF_GRADIENTS = 'TF_GRADIENTS'
+  GRADIENTS_SPEED = 'GRADIENTS_SPEED'
+  GRADIENTS_MEMORY = 'GRADIENTS_MEMORY'
+  GRADIENTS_COLLECTION = 'GRADIENTS_COLLECTION'
+  ADAM_COMPUTE_GRADIENTS = 'ADAM_COMPUTE_GRADIENTS'
+
+  @classmethod
+  def all(cls):
+    return [
+        cls.PLAIN_ADAM, cls.TF_GRADIENTS, cls.GRADIENTS_SPEED, cls.GRADIENTS_MEMORY,
+        cls.GRADIENTS_COLLECTION, cls.ADAM_COMPUTE_GRADIENTS
+      ]
+
 
 class UNet(object):
   def __init__(self, filters=32, n_conv=2, dropout=0, batch_norm=False, gradient_type=GradientType.PLAIN_ADAM):
@@ -20,6 +29,7 @@ class UNet(object):
     self._inference_op = self.build_model(self._input, filters, n_conv, dropout, batch_norm, self._training)
 
     with tf.variable_scope('train'):
+      print('GRADIENT TYPE:', gradient_type)
       self._loss_op = tf.losses.softmax_cross_entropy(self._labels, logits=self._inference_op)
       update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
       with tf.control_dependencies(update_ops):
@@ -27,12 +37,17 @@ class UNet(object):
         grads = None
         if gradient_type == GradientType.PLAIN_ADAM:
           self._train_op = optimizer.minimize(self._loss_op)
+        elif gradient_type == GradientType.ADAM_COMPUTE_GRADIENTS:
+          grads = optimizer.compute_gradients(self._loss_op)
+          self._train_op = optimizer.apply_gradients(grads)
+          return
         elif gradient_type == GradientType.TF_GRADIENTS:
-          grads = tf.gradients(self._loss_op, tf.trainable_variables())
+          grads = tf.gradients(self._loss_op, tf.trainable_variables(),
+            gate_gradients=True)
         elif gradient_type == GradientType.GRADIENTS_SPEED:
           grads = gradients_speed(self._loss_op, tf.trainable_variables())
         elif gradient_type == GradientType.GRADIENTS_MEMORY:
-          grads = gradients_memory(self._loss_op, tf.trainable_variables())
+          grads = gradients_memory(self._loss_op, tf.trainable_variables(),gate_gradients=True)
         elif gradient_type == GradientType.GRADIENTS_COLLECTION:
           grads = gradients_collection(self._loss_op, tf.trainable_variables())
         if grads:
