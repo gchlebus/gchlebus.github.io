ce475b4fe8a3873d396433c45723e9d7a7489690
gchlebus <gchlebus@gmail.com>
2018-02-02 09:56:58 +0100

    Update.

--- code/loss-functions/main.py
+++ code/loss-functions/main.py
@@ -12,15 +12,22 @@ def parse_args():
   parser.add_argument('-t', type=float, default=0.5)
   return parser.parse_args()
 
-if __name__ == '__main__':
-  args = parse_args()
-  model = UNet()
-  sess = tf.Session()
-  sess.run(tf.global_variables_initializer())
+def get_data(threshold):
   input_shape = [1] + UNet.INPUT_SHAPE
   input_batch = np.random.rand(*input_shape)
-  print('Class distribution bg: %d%% fg: %d%%' % (args.t*100, (1-args.t)*100))
-  output_batch = np.concatenate([input_batch<=args.t, input_batch>args.t], axis=-1).astype(np.float32)
+  output_batch = np.concatenate([input_batch<=threshold, input_batch>threshold], axis=-1).astype(np.float32)
+  bg_count = np.sum(output_batch[...,0])
+  fg_count = np.sum(output_batch[...,1])
+  bg_percent = 100 * bg_count / (bg_count + fg_count)
+  print('Class distribution bg: %.2g%% fg: %.2g%%' % (bg_percent, 100-bg_percent))
+  return input_batch, output_batch
+
+if __name__ == '__main__':
+  args = parse_args()
+  model = UNet(filters=4)
+  sess = tf.Session()
+  sess.run(tf.global_variables_initializer())
+  input_batch, output_batch = get_data(args.t)
   for i in range(args.i):
     _, cce_loss, cce_grad, dice_fg_loss, dice_fg_grad, dice_loss, dice_grad = model.train(sess, input_batch, output_batch)
     print('[%d] cce_loss: %.3g cce_norm: %.3g dice_fg_loss: %.3g dice_fg_norm: %.3g dice_loss: %.3g dice_norm: %.3g' % 
