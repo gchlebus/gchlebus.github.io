c51093fa410e68db07be603f60d879e81123c299
gchlebus <gchlebus@gmail.com>
2018-06-05 19:43:50 +0200

    Add code for gradient averaging experiments.

---
+++ code/gradient-averaging/conv_net.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+__author__ = 'gchlebus'
+
+import tensorflow as tf
+import numpy as np
+
+class ConvNet(object):
+  def __init__(self, dropout=0.5):
+    self._input = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+    self._training = tf.placeholder_with_default(False, shape=[])
+    self._labels = tf.placeholder(tf.float32, shape=[None, 10])
+
+    out = self._input
+    if dropout:
+      out = tf.layers.dropout(out, rate=0.2 if dropout > 0.2 else dropout, training=self._training)
+    out = self.conv2d(out, filters=6, name='layer0') # 24
+    out = tf.layers.max_pooling2d(out, pool_size=2, strides=2) # 12
+    if dropout:
+      out = tf.layers.dropout(out, rate=dropout, training=self._training)
+    out = self.conv2d(out, filters=16, name='layer1') # 8
+
+    out = tf.layers.max_pooling2d(out, pool_size=2, strides=2) # 4
+    if dropout:
+      out = tf.layers.dropout(out, rate=dropout, training=self._training)
+    out = tf.contrib.layers.flatten(out)
+    out = self.dense(out, filters=120, name='layer2')
+    if dropout:
+      out = tf.layers.dropout(out, rate=dropout, training=self._training)
+    out = self.dense(out, filters=84, name='layer3')
+    if dropout:
+      out = tf.layers.dropout(out, rate=dropout, training=self._training)
+    out = self.dense(out, filters=10, name='layer4')
+    self._inference_op = out
+
+    correct = tf.equal(tf.argmax(self._labels, 1), tf.argmax(self._inference_op, 1))
+    self._accuracy_op = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+  @classmethod
+  def conv2d(cls, input, filters, name=None):
+    return tf.layers.conv2d(input, filters=filters, kernel_size=5, activation=tf.nn.relu, name=name)
+
+  @classmethod
+  def dense(cls, input, filters, name=None):
+    return tf.layers.dense(input, filters, activation=tf.nn.relu, name=name)
+
+  def setup_train(self, average_gradients=1, lr=1e-3):
+    self._average_gradients = average_gradients
+    self._loss_op = tf.losses.softmax_cross_entropy(onehot_labels=self._labels, logits=self._inference_op)
+    optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+    if average_gradients == 1:
+      self._train_op = optimizer.minimize(self._loss_op)
+    else:
+      grads_and_vars = optimizer.compute_gradients(self._loss_op)
+      avg_grads_and_vars = []
+      self._grad_placeholders = []
+      for grad, var in grads_and_vars:
+        grad_ph = tf.placeholder(grad.dtype,  grad.shape)
+        self._grad_placeholders.append(grad_ph)
+        avg_grads_and_vars.append((grad_ph, var))
+      self._grad_op = [x[0] for x in grads_and_vars]
+      self._train_op = optimizer.apply_gradients(avg_grads_and_vars)
+      self._gradients = []
+
+  def train(self, session, input_batch, output_batch):
+    feed_dict = {
+      self._input: input_batch,
+      self._labels: output_batch,
+      self._training: True
+    }
+    if self._average_gradients == 1:
+      loss, _ = session.run([self._loss_op, self._train_op], feed_dict=feed_dict)
+    else:
+      loss, grads = session.run([self._loss_op, self._grad_op], feed_dict=feed_dict)
+      self._gradients.append(grads)
+      if len(self._gradients) == self._average_gradients:
+        for i, placeholder in enumerate(self._grad_placeholders):
+          feed_dict[placeholder] = np.stack([g[i] for g in self._gradients], axis=0).mean(axis=0)
+        session.run(self._train_op, feed_dict=feed_dict)
+        self._gradients = []
+    return loss
+
+  def evaluate(self, session, input_batch, output_labels):
+    feed_dict = {
+      self._input: input_batch,
+      self._labels: output_labels
+    }
+    return session.run(self._accuracy_op, feed_dict=feed_dict) * 100
+
+
---
+++ code/gradient-averaging/main.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+__author__ = 'gchlebus'
+
+from conv_net import ConvNet
+import numpy as np
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_DATA', one_hot=True)
+
+def parse_args():
+  import argparse
+  parser = argparse.ArgumentParser(description='Gradient Averaging Example.')
+  parser.add_argument('-n', '--average-gradients', type=int, default=1, help='Average gradients every n iterations.')
+  parser.add_argument('-b', '--batch-size', type=int, default=100, help='Mini-batch size.')
+  parser.add_argument('-i', '--iterations', type=int, default=50000, help='Max iteration count.')
+  parser.add_argument('-r', '--reps', type=int, default=10, help='Experiment runs.')
+  parser.add_argument('-v', '--verbose', action='store_true')
+  return parser.parse_args()
+
+def run_experiment(average_gradients, batch_size, iterations, verbose):
+  batch_size = batch_size
+  tf.reset_default_graph()
+  net = ConvNet()
+
+  validation_batch = mnist.test.images
+  val_count = validation_batch.shape[0]
+  validation_batch = np.reshape(validation_batch, (val_count, 28, 28, 1))
+  validation_labels = mnist.test.labels
+
+  net.setup_train(average_gradients=average_gradients)
+  training_log = []
+  with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(iterations):
+      batch = mnist.train.next_batch(batch_size)
+      input_batch = np.reshape(batch[0], (batch_size, 28, 28, 1))
+      loss = net.train(sess, input_batch, batch[1])
+      if (i+1) % 100 == 0:
+        accuracy = net.evaluate(sess, validation_batch, validation_labels)
+        training_log.append((accuracy, i+1))
+        if args.verbose:
+          print('[{:d}/{:d}] loss: {:.3g}, accuracy: {:.3g}%'.format(i+1, iterations, loss, accuracy))
+    accuracy = net.evaluate(sess, validation_batch, validation_labels)
+    training_log.append((accuracy, iterations))
+    best = sorted(training_log, key=lambda x: x[0], reverse=True)[0]
+    print('Training finished. Best accuracy: {:.3g} at iteration {:d}.'.format(best[0], best[1]))
+    return best[0]
+
+if __name__ == '__main__':
+  args = parse_args()
+  print('EXPERIMENT STARTED')
+  print(args)
+  accuracies = [run_experiment(args.average_gradients, args.batch_size, args.iterations, args.verbose)
+      for i in range(args.reps)]
+  print('EXPERIMENT FINISHED')
+  print('mean accuracy ({:d} runs): {:.3g} +/- {:.3g}'.format(args.reps, np.mean(accuracies), np.std(accuracies)))
