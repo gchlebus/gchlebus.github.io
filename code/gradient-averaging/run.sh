746b6c18b273bb141d1c6047ea8de836e073963a
Grzegorz Chlebus <gchlebus@gmail.com>
2018-06-05 21:41:44 +0200

    Add experiment results.

--- code/gradient-averaging/run.sh
+++ code/gradient-averaging/run.sh
@@ -1,10 +1,10 @@
 #! /bin/zsh
 
-python main.py --average-gradients=1 --batch-size=100 --iterations=1000 --verbose
+python main.py --average-gradients=1 --batch-size=100 --iterations=1000
 
-python main.py --average-gradients=5 --batch-size=20 --iterations=5000 --verbose
+python main.py --average-gradients=5 --batch-size=20 --iterations=5000
 
-python main.py --average-gradients=10 --batch-size=10 --iterations=10000 --verbose
+python main.py --average-gradients=10 --batch-size=10 --iterations=10000
 
-python main.py --average-gradients=20 --batch-size=5 --iterations=20000 --verbose
+python main.py --average-gradients=20 --batch-size=5 --iterations=20000
 
